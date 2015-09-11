4b7a5d1bee6b5e30eb9d0461c842e86c05a3a0ac
jbm02 <jean-baptiste.michel02@orange.fr>
2015-09-11 14:50:52 +0200

    WikiMNV-Core-Admin-serviceProfil-cli

--- Core/Admin-serviceProfil-cli/src/main/java/fr/cap/wikimnv/service/CountArticlesEcris.java
+++ Core/Admin-serviceProfil-cli/src/main/java/fr/cap/wikimnv/service/CountArticlesEcris.java
@@ -26,7 +26,7 @@ import javax.xml.bind.annotation.XmlType;
  * 
  */
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "countArticlesEcris", propOrder = {
+@XmlType(name = "countArticlesEcris", propOrder = { 
     "arg0"
 })
 public class CountArticlesEcris {
